e4bd0c7a4bce992adcdcd3503e5af7c4d77f9944
Jason Ivan Estrada Aguirre <157194045+IvanJason32@users.noreply.github.com>
2024-10-01 19:05:53 -0600

    Se creo la lista de criptomonedas, se agrego Axios y se creo el archivo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css"
+
 function App() {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [criptos, setCriptos] = useState();
+
+  // useEffect(() => {
+  //   fetch(`${API_URL}assets`)
+  //     .then((resp) => resp.json())
+  //     .then((data) => {
+  //       setCriptos(data.data);
+  //     })
+  //     .catch(() => {
+  //       console.error("La petición falló");
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}assets`)
+      .then((data) => {
+        setCriptos(data.data.data);
+      })
+      .catch(() => {
+        console.error("La petición falló");
+      });
+  }, []);
+
+  if (!criptos) return <span>Cargando...</span>;
 
   return (
-   <h1>Hola mundo</h1>
-  )
+    <>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        {criptos.map(({ name, priceUsd, id }) => (
+          <li key={id}>
+            Nombre: {name} Precio: {priceUsd}
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 }
 
-export default App
+export default App;
